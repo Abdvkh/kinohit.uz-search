b189fb431386f7b1964e46b91ea628a68df5c814
Abdvkh <e-address@bk.ru>
2020-05-26 00:01:58 +0500

    Added multi-keywords search; Implemented interface; Added cartoons search;

--- README.md
+++ README.md
@@ -12,37 +12,60 @@ These libraries are:
 
 ## Usage
 1. In command line you run 'search.py'
-2. Enter type of movie(e.g. serials)
-3. How many pages it should go through
-4. Details for search(if you pass Комедия all the films containing this word will be returned)
+2. Enter command code(e.g. 's' - to search)
+3. Enter type of movie(e.g. serials)
+4. How many pages it should go through
+5. Details for search(if you pass Комедия all the films containing this word will be returned)
 
 ### Example
 `python search.py`
 
 ```
-Enter type of movies. Available now: films, serials
->serials
+========== INTERFACE ==========
+Welcome to kinohit.uz searching engine!
+Use command during the program:
+[s]earch - to start searching
+[q]uit - to quit the program
+
+Enter the command of the program: s
+Enter type of movies. Available now: films, serials, cartoons
+>cartoons
 How many pages do you want us to through?
->2
-Details the film include: Комедия
+>4
+Details the film include, add by comma. For example: Комедия, Драма.
+>6+, Супер
+========== RESULTS START ==========
 
-Name: Соник Х
-Description: Адаптация знаменитой видеоигры компании Sega....
-Categories: Аниме  Мелодрама  Боевик  Комедия  Мультфильм
-Link: 'https://kinohit.uz/serialy-na-russkom-v-tasix/16860-sonik-h.html'
-Last episode: 26 серия 3 сезон
+Name: Супер Пёс и Турбо Кот
+Description: Запущенный в космос его владельцем ученым в 1969 году, верный пёс Бадди терпит крушение в наши дни в небольшом городке, ...
+Categories: Фэнтези Комедия Мультфильм Семейный
+Link: 'https://kinohit.uz/russian-film-kino-tasix-tas-uz-uzbekistan/fentezi/16846-super-pes-i-turbo-kot.html'
+Age: 6+
+Ratings: IMDb - 7.2 Kinopoisk - 0
+Quality: SD
 
-Name: Великая
-Description: Молодая дочь немецкого князя София Августа Фредерика Ангальт...
-Categories: Исторический  Комедия
-Link: 'https://kinohit.uz/serialy-na-russkom-v-tasix/16851-velikaya.html'
-Last episode: 10 серия 1 сезон
 
+Name: Форма голоса
+Description: Молодой человек Сёя Исида внезапно осознаёт, что в школе умудрился превратить жизнь одноклассницы Сёко в ад только потом...
+Categories: Аниме Мультфильм
+Link: 'https://kinohit.uz/russian-film-kino-tasix-tas-uz-uzbekistan/anime/16803-forma-golosa.html'
+Age: 16+
+Ratings: IMDb - 8.2 Kinopoisk - 8.0
+Quality: SD
+
+Name: Риф. Новые приключения
+Description: Это захватывающая и смешная история отважного Алекса — рыбы-попугая, который мечтает стать Супергероем. Он получает свои...
+Categories: Мультфильм Приключения
+Link: 'https://kinohit.uz/russian-film-kino-tasix-tas-uz-uzbekistan/multfilm/16610-rif-novye-priklyucheniya.html'
+Age: 0+
+Ratings: IMDb - 4.3 Kinopoisk - 0
+Quality: SD
 ...
 ```
+Let's observe results:
+1. "Супер Пёс и Турбо Кот" - coincidence in 'Age'
+2. "Форма голоса" - coincidence in 'Age'
+3. "Риф. Новые приключения" - coincidence in 'Description'
 
 
-
-
-
-My telegram for feedback: t.me/Abduvakhidov
+My telegram for feedback: https://t.me/Abduvakhidov
--- search.py
+++ search.py
@@ -4,9 +4,14 @@ try:
 except ModuleNotFoundError:
     print('Firstly you have to download dependencies by "pip install -r requirements.txt"')
 
+site = 'https://kinohit.uz/'
+
+hr = "="*10 + "{}" + "="*10
+
 urls = {
-    'films': 'https://kinohit.uz/russian-film-kino-tasix-tas-uz-uzbekistan/',
-    'serials': 'https://kinohit.uz/serialy-na-russkom-v-tasix/'
+    'films': site + 'russian-film-kino-tasix-tas-uz-uzbekistan/',
+    'serials': site + 'serialy-na-russkom-v-tasix/',
+    'cartoons': site + 'russian-film-kino-tasix-tas-uz-uzbekistan/multfilm/'
 }
 
 def get_stripped_text(component):
@@ -16,27 +21,58 @@ def get_scraped(url, session):
     page = session.get(url)
     return BS(page.content, 'html.parser')
 
+def content_includes(content, details):
+    keywrds = details.split(',')
+    coincidences = []
+    if details is not None:
+        for word in keywrds:
+            coincidences.append(word in content)
+
+    counts = coincidences.count(True)
+    if counts >= 1:
+        return True
+    return False
+
 def get_films(scraped_page, details, description_length=120):
     films = scraped_page.find_all('a', class_='popover-dismissible-serial')
-
     for data in films:
-        link = data['href']
+        link = data['href'].strip()
         content = data['data-content']
-        if details in content:
+        if content_includes(content, details):
             result = BS(content, 'html.parser')
-            msg = f"Name: {result.h3.get_text().strip()}"\
-                + f"\nDescription: {get_stripped_text(result.p)[:description_length]}..."\
-                + f"\nCategories: {', '.join([get_stripped_text(categ) for categ in result.select('div.box_info_cat_movie')])}"\
-                + f"\nLink: '{link.strip()}'"\
-                + f"\n{'Quality' if type == 'films' else 'Last episode'}: {get_stripped_text(data.parent.parent.select('div.kachestvo_short_story')[0])}\n\n"
+
+            name = result.h3.get_text().strip()
+            descr = get_stripped_text(result.p)[:description_length]
+            categories = [get_stripped_text(categ) \
+                          for categ in result.select('div.box_info_cat_movie')]
+            age = get_stripped_text(result.find('div',
+                                                class_='box_info_age'))
+            ratings = [get_stripped_text(rating)\
+                        for rating in result.find_all('div',
+                                                        class_='text-rate')]
+            extra = 'Quality' if type != 'serials' else 'Last episode'
+            quality_or_episode = get_stripped_text(
+                data.parent.parent.select('div.kachestvo_short_story')[0]
+            )
+
+            msg = f"\nName: {name}"\
+                + f"\nDescription: {descr}..."\
+                + f"\nCategories: {', '.join(categories)}"\
+                + f"\nLink: '{link}'"\
+                + f"\nAge: {age}"\
+                + f"\nRatings: IMDb - {ratings[0]} Kinopoisk - {ratings[1] if len(ratings)==2 else '0'}"\
+                + f"\n{extra}: {quality_or_episode}\n"
 
             print(msg)
 
 def get_russian(type):
     url = urls[type]
+
     pages_count = int(input('How many pages do you want us to through?\n>'))
-    details = input('Details the film include: ')
+    details = input('Details the film include, add extras by comma. For example: 6+, Комедия.\n>')
+
     s = requests.Session()
+    print(hr.format(' RESULTS START '))
 
     first_page_scraped = get_scraped(url, s)
     get_films(first_page_scraped, details)
@@ -45,7 +81,18 @@ def get_russian(type):
         for i in range(2, int(pages_count)+1):
             page_scraped = get_scraped(url + f'page/{str(i)}/', s)
             get_films(page_scraped, details)
+    print(hr.format(' RESULTS END '))
 
 if __name__ == '__main__':
-    type = input(f"Enter type of movies. Available now: {', '.join(list(urls.keys()))}\n>")
-    get_russian(type)
+    msg = hr.format(' INTERFACE ') + '\nWelcome to kinohit.uz searching engine!\
+\nUse command during the program:\
+\n[s]earch - to start searching\
+\n[q]uit - to quit the program'
+    command = input(f'{msg}\n\nEnter the command of the program: ')
+    while not (command.lower() in ['q', 'quit', 'exit', '^C']):
+        search_types = ', '.join(list(urls.keys()))
+        if command.lower() in ['s', 'search', 'start']:
+            type = input(f"Enter type of movies. Available now: {search_types}\n>")
+            get_russian(type)
+
+        command = input(f'{msg}\n\nEnter the command of the program: ')
